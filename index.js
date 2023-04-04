eaae95f6fed50f493e0d3f7a85eeafccf0dfe0a6
Ganpat <ganpat@addwebsolution.in>
2023-04-04 11:22:47 +0530

    test 4

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  const clientIps = req.headers['x-forwarded-for'];
+  const clientIp = clientIps ? clientIps.split(',')[0] : req.socket.remoteAddress;
   res.send('Client IP: ' + clientIp);
 });
 
