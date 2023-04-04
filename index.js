705f8206ea782675269b62a30c7005933dee3509
Ganpat <ganpat@addwebsolution.in>
2023-04-04 11:16:15 +0530

    test2

--- index.js
+++ index.js
@@ -2,14 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  console.log("🚀 -> 🌏 ~ file: index.js:5 ~ req:", req)
-  console.log("🚀 -> 🌏 ~ file: index.js:6 ~ req.socket.remoteAddress:", req.socket.remoteAddress)
-  console.log("🚀 -> 🌏 ~ file: index.js:7 ~ req.ip:", req.ip)
-  res.send('your IP is: ' + req.ip,req.socket.remoteAddress,req.ip);
+  const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  res.send('Client IP: ' + clientIp);
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('server running on port: ' + PORT);
+  console.log('Server running on port: ' + PORT);
 });
-   
